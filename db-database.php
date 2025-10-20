bc8469ddaee69f7676f9fb5ca6ac8acb77bbef64
tobnyl0 <tonyl9954@usn.no>
2025-10-20 13:44:02 +0200

    s

---
+++ db-database.php
@@ -0,0 +1,13 @@
+<?php  /* db-tilkobling */
+/*
+/*  Programmet foretar tilkobling til database-server og valg av database
+*/
+$host = getenv('DB_HOST');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+$database = getenv('DB_DATABASE');
+
+ $db=mysqli_connect($host,$username,$password,$database) or die ("ikke kontakt med database-server");
+    /* tilkobling til database-serveren utført */
+ ?>
+ 
