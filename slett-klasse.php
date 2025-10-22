611e9a714ca01e6af58634827e62f1cc357e89ab
tobnyl0 <tonyl9954@usn.no>
2025-10-22 12:16:51 +0200

    sw

--- slett-klasse.php
+++ slett-klasse.php
@@ -41,7 +41,7 @@
               mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; slette data i databasen");
                 /* SQL-setning sendt til database-serveren */
 		
-              print ("F&oslash;lgende poststed er n&aring; slettet: $klassenavn  <br />");
+              print ("F&oslash;lgende klasse er n&aring; slettet: $klassenavn  <br />");
             }
         }
     }
